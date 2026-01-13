be5c4927a56443d53cc55fa71579372e0c02019d
José Antonio García Solano <joseagarsol@gmail.com>
2026-01-13 10:54:51 +0100

    refactor: Implement validation ad error handling in server action

--- src/features/invoices/actions.ts
+++ src/features/invoices/actions.ts
@@ -3,18 +3,52 @@
 import { db } from "@/db";
 import { invoices } from "@/db/schema";
 import { revalidatePath } from "next/cache";
+import { createInvoiceSchema } from "./schemas";
 
-export async function createInvoice(formData: FormData) {
-  const amountStr = formData.get("amount") as string;
-  const status = formData.get("status") as string;
+export type ActionState = {
+  status: "idle" | "success" | "error";
+  message: string;
+  errors?: {
+    amount?: string[];
+    status?: string[];
+  };
+};
 
-  const amount = Math.round(parseFloat(amountStr) * 100);
+export async function createInvoice(
+  prevState: ActionState,
+  formData: FormData
+) {
+  const rawData = {
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  };
 
-  await db.insert(invoices).values({
-    amount,
-    status,
-  });
+  const validateFields = createInvoiceSchema.safeParse(rawData);
 
-  revalidatePath("/");
+  if (!validateFields.success) {
+    return {
+      status: "error",
+      message: "Por favor, revisa los campos del formulario.",
+      errors: validateFields.error.flatten().fieldErrors,
+    };
+  }
+
+  try {
+    await db.insert(invoices).values({
+      amount: Math.round(validateFields.data.amount * 100),
+      status: validateFields.data.status,
+    });
+
+    revalidatePath("/");
+
+    return {
+      status: "success",
+      message: "Factura creada correctamente.",
+    };
+  } catch (error) {
+    return {
+      status: "error",
+      message: "Error en la base de datos. Inténtalo de nuevo.",
+    };
+  }
 }
-
